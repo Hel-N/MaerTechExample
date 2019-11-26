3481e1af528c7d33568fd1b3da8495fdc52a61fa
Venotance <stas_volkov_2013@mail.ru>
2019-11-26 19:41:00 +0300

    Delete volkov.cpp, modified source.cpp

--- source.cpp
+++ source.cpp
@@ -10,10 +10,14 @@ int main() {
     vector<int> v(500);
     generate(v.begin(), v.end(), rand);
     
+    int res = 0;
+    
     for(int i = 0; i< v.size(); ++i){
         cout << v[i] << " ";    
-    
+        res += v[i];
     }
+    
+    cout << endl << "Result: " << res << endl;
     
     return 0;
 }
